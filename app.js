3e6e62d35a3ee6bc1685e357860ad974b9b70aaf
filipe custodio <filipe.l.custodio@gmail.com>
2019-03-27 19:32:27 +0000

    Updating the budget: Budget controller

--- app.js
+++ app.js
@@ -21,8 +21,21 @@ var budgetController = (function() {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   }
+
+  var calculateTotal = function(type) {
+    var sum = 0;
+
+    data.allItems[type].forEach(function(cur) {
+      sum += cur.value;
+    });
+
+    data.totals[type] = sum
+
+  };
 
   return {
     addItem: function(type, des, val) {
@@ -49,6 +62,33 @@ var budgetController = (function() {
 
       // return the new element
       return newItem;
+    },
+
+    calculateBudget: function() {
+
+      // calculate total income and expenses
+      calculateTotal('exp');
+      calculateTotal('inc');
+
+      // calculate the budget: income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
+
+      // calculate the percentage of income that we spent
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        data.percentage = -1;
+      }
+
+    },
+
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      };
     },
 
     testing: function() {
@@ -151,11 +191,13 @@ var controller = (function(budgetCtrl, UICtrl){
 
   var updateBudget = function() {
     // 1. Calculate the budget
+    budgetCtrl.calculateBudget();
 
     // 2. Return the budget
+    var budget = budgetCtrl.getBudget();
 
     // 3. Display the budget
-
+    console.log(budget);
   };
 
   var ctrlAddItem = function() {
